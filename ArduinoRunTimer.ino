e6aa638b18a92615a3a7680cc3e814f14245d508
EvanDSays <evandavis@gmail.com>
2022-04-20 21:40:49 -0500

    Update ArduinoRunTimer.ino

--- ArduinoRunTimer.ino
+++ ArduinoRunTimer.ino
@@ -3,9 +3,13 @@
 // Select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);            
 
-int runCounter = 0;
-int adc_key_in  = 0;
-long runStartTime = 0;
+int runCounter = 0; //Keeps track of the total number of runs
+bool paused = false; //Holds the state of device (paused or unpaused)
+int adc_key_in  = 0; //Holds button value from sensor
+long runStartTime = 0; //Keeps track of when each run starts
+long pauseStartTime = 0; //Keeps track of when the device is paused
+long pauseMillis = 0; //The amount of milliseconds that the device has been paused
+
 
 #define btnRIGHT  0
 #define btnUP     1
@@ -15,16 +19,16 @@ long runStartTime = 0;
 #define btnNONE   5
 int read_LCD_buttons()
 {
- adc_key_in = analogRead(0);      // read the value from the sensor 
+ adc_key_in = analogRead(0); //Read the value from the sensor 
  // The buttons when read are centered at these values: 0, 144, 329, 504, 741
  // We add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ if (adc_key_in > 1000) return btnNONE; // First option for speed reasons since it will be the most likely result
  if (adc_key_in < 50)   return btnRIGHT;  
  if (adc_key_in < 250)  return btnUP; 
  if (adc_key_in < 450)  return btnDOWN; 
  if (adc_key_in < 650)  return btnLEFT; 
  if (adc_key_in < 850)  return btnSELECT;  
- return btnNONE;  // when all others fail, return this
+ return btnNONE;  // When all others fail, return this
 }
 
 //Formats milliseconds into a H:M:S string
@@ -38,16 +42,16 @@ String formatMilli(long mil){
     s = mil/1000;
     if(s>9) return (String)"0:" + s;
     else    return (String)"0:0" + s;
-    
   }
+  
   //less than 1 hour, return minutes:seconds
   else if (mil < 3600000){
     m = mil/1000/60;
     s = mil/1000 - (m*60);
     if(s>9) return (String)m + ":" + s;
-    else    return (String)m + ":0" + s;
-    
+    else    return (String)m + ":0" + s;  
   }
+  
   //greater than 1 hour, return hours:minutes:seconds
   else{
     h = mil/1000/60/60;
@@ -58,38 +62,64 @@ String formatMilli(long mil){
   }
 }
 
+//runs at startup
 void setup() {
-  // set up the LCDs number of columns and rows
-  lcd.begin(16, 2);
-  runCounter = 0;
+  lcd.begin(16, 2); //Sets up the LCDs number of columns and rows
+  lcd.setCursor(0, 0);
+  lcd.print("Run#1"); //Print Run#1 to top left of screen
 }
 
 void loop() {
 
-  lcd.setCursor(0, 0);
-  lcd.print((String)"Run#" + (runCounter+1));
-  
-  String currentRunDuration = (String)" C-" + formatMilli(millis() - runStartTime);
-  lcd.setCursor(16 - currentRunDuration.length(), 0);
-  lcd.print(currentRunDuration);
-  
-  String totalTime = (String)" T-" + formatMilli(millis());
-  lcd.setCursor(16 - totalTime.length(),1);
-  lcd.print(totalTime);
-  
-  int buttonNum = read_LCD_buttons();
+  //If the device is not paused, continue to print information to screen
+  if(not paused){
+    //print current run duration
+    String currentRunDuration = (String)" C-" + formatMilli(millis() - pauseMillis - runStartTime);
+    lcd.setCursor(16 - currentRunDuration.length(), 0);
+    lcd.print(currentRunDuration);
 
-  //once any button (other than reset) has been clicked
-  if (buttonNum < 5) {
+    //print total time
+    String totalTime = (String)" T-" + formatMilli(millis() - pauseMillis);
+    lcd.setCursor(16 - totalTime.length(),1);
+    lcd.print(totalTime);
+  }
+  
+  //See if any buttons have been pressed
+  int buttonNum = read_LCD_buttons(); 
+
+  //If DOWN or UP are pressed, pause or unpause the device
+  if (buttonNum == btnDOWN or buttonNum == btnUP){
+    if(paused){
+      //add the amount of paused time to the pause counter
+      pauseMillis = pauseMillis + (millis() - pauseStartTime);
+      paused = false;
+    }
+    else{ //if not paused
+      paused = true;
+      //keep track of when pause was pressed
+      pauseStartTime = millis();
+    }
+    //prevent multiple clicks
+    delay(500);
+  }
+  
+  //If SELECT or LEFT are pressed and it is not paused, start a new run
+  else if (not paused and (buttonNum == btnSELECT or buttonNum == btnLEFT)) {
     runCounter++;
+
+    //print run number
+    lcd.setCursor(0, 0);
+    lcd.print((String)"Run#" + (runCounter+1));
     
     //Reset current run time
-    runStartTime = millis();
+    runStartTime = millis() - pauseMillis;
 
     //Recalculate average run time and print to screen
     lcd.setCursor(0, 1);
-    String averageTime = (String)"A-" + formatMilli(millis()/runCounter);
+    String averageTime = (String)"A-" + formatMilli((millis()-pauseMillis)/runCounter);
     lcd.print(averageTime);
+
+    //prevent multiple clicks
     delay(500);
   }
   
